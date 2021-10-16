1017e9b602bd60638b08471dfd5689db2eed7f3a
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-10-16 21:19:08 +0900

    renew

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+  // let x = 1;
+  // let y = x;
+  // x = 5;
+  // console.log(x); // 5
+  // console.log(y); // 1
+
+  let x = [1, 2];
+  let y = x;
+  x[0] = 5;
+  console.log(x); // [5, 2]
+  console.log(y); // [1, 2]
+}

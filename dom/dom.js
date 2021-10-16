0ec3ecc3bcf8432afcbb2459e3850fac61fd88ca
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-10-17 00:36:52 +0900

    renew

---
+++ dom/dom.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// {
+//     function update() {
+//         // document.querySelector('h1').textContent = 'Changed!';
+//         document.getElementById('target').textContent = 'Changed!';
+//     }
+//     setTimeout(update, 1000);
+// }
+
+{
+    function update() {
+        // document.getElementById('target').textContent = 'Changed!';
+        // document.querySelectorAll('p')[1].textContent = 'Changed!';
+        document.querySelectorAll('p').forEach((p, index) => {
+            p.textContent = `${index}番目のpです`;
+        });
+    }
+
+    setTimeout(update, 1000);
+}
+
